ec90b48ebe2a0d2153166ab0e3b2a67464496824
JacklineKigombe <muthoninamunyu@gmail.com>
2021-10-23 14:19:31 +0300

    Added a new file assignment 1 for Lecture 9

---
+++ assignment1.py
@@ -0,0 +1,9 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 100
+>>> type(a)
+<class 'int'>
+>>> b = 100.0
+>>> type(b)
+<class 'float'>
+>>> 
