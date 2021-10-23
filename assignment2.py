f798fa3cbb0acfbd82134bdb0e29d88af7715a7e
JacklineKigombe <muthoninamunyu@gmail.com>
2021-10-23 14:22:24 +0300

    Added 3 assignment files for Lecture 9

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+#Assignment2
+
+def main():
+    num = int(input("Enter a number: "))
+    result = num**2
+    print("The square of ",num," is ",result)
+
+
+main()
---
+++ assignment3.py
@@ -0,0 +1,10 @@
+#Assignment3
+
+def main():
+    import math
+    num = int(input("Enter a number: "))
+    result = math.sqrt(num)
+    print("The square of ",num," is ",result)
+
+
+main()
