a12651ce68650fbffb566a23870b6f20c67f1ffb
JacklineKigombe <muthoninamunyu@gmail.com>
2021-10-25 22:14:59 +0300

    Added 3 more assignment files for Lecture 9

--- assignment5.py
+++ assignment5.py
@@ -1,11 +1,11 @@
-#This function computes the factorial
+#This function computes the power of 5
 #of a number entered by the user
 
 def factor():
     n = int(input("Please enter a whole number: "))
     fact = 1
     for factor in range(1, n+1):
-        fact = fact * factor
+        fact = fact * 5
     print("The factorial of ",n," is",fact)
 
 
---
+++ assignment6.py
@@ -0,0 +1,11 @@
+#Calculating the sum of 1 to 1000
+
+def main():
+    fact = 0
+    for factor in range (1,1001):
+        fact = fact + factor
+
+    print(fact)
+
+
+main()
